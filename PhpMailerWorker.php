f5ad0c37ffc0c2e3a2e9a737ebcd9fb872e2928f
EU <riodelacruz033@gmail.com>
2024-11-15 08:40:23 +0800

    sir ignasRevision program management and table orders

--- PhpMailerWorker.php
+++ PhpMailerWorker.php
@@ -20,8 +20,8 @@ function mailer($subjectType,$bodyMessage, $emailAddress){
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth = true;                                   //Enable SMTP authentication
-        $mail->Username = 'cc.riocarl.delacruz@cvsu.edu.ph';                     //SMTP username
-        $mail->Password = 'lmsuohilwsildxex';                               //SMTP password
+        $mail->Username = '';                     //SMTP username
+        $mail->Password = '';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            // ENCRYPTION_SMTPS 465 Enable implicit TLS encryption
         $mail->Port = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
