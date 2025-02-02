fc7c5c6bfa70dbb88ebe62a2ee460ecaff770cb9
EU <riodelacruz033@gmail.com>
2025-02-02 12:12:24 +0800

    img path

--- src/home.php
+++ src/home.php
@@ -7,13 +7,13 @@ if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'
 
 <section class="py-16">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-            <img class="rounded-2xl  border-gray-200 shadow-2xl oject-cover w-full h-30 md:h-96 lg:h-50 md:w-40 md:rounded-none md:rounded-s-lg" src="/Reposync-main/src/assets/4.png" alt="">
+            <img class="rounded-2xl  border-gray-200 shadow-2xl oject-cover w-full h-30 md:h-96 lg:h-50 md:w-40 md:rounded-none md:rounded-s-lg" src="assets/4.png" alt="">
         <div class="mt-12 ml-6 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
 
             <!-- Mission Card -->
             <div class="max-w-sm bg-white border border-gray-200 rounded-2xl shadow-2xl dark:bg-gray-800 dark:border-gray-700">
                 <a>
-                    <img class="rounded-t-xl" src="/Reposync-main/src/assets/1.png" alt="Mission Image" />
+                    <img class="rounded-t-xl" src="assets/1.png" alt="Mission Image" />
                 </a>
                 <div class="p-5">
                     <p class="mb-3 font-mono text-gray-700 dark:text-gray-400 text-justify">
@@ -24,7 +24,7 @@ if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'
 
             <!-- Vision Card -->
             <div class="max-w-sm bg-white border border-gray-200 rounded-2xl shadow-2xl dark:bg-gray-800 dark:border-gray-700">
-                <img class="rounded-t-lg" src="/Reposync-main/src/assets/2.png" alt="Vision Image" />
+                <img class="rounded-t-lg" src="assets/2.png" alt="Vision Image" />
                 <div class="p-5">
                     <p class="mb-3 font-mono text-gray-700 dark:text-gray-400 text-justify">
                         The premier university in historic Cavite globally recognized for excellence in character development, academics, research, innovation, and sustainable community engagement.
@@ -34,7 +34,7 @@ if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'
 
             <!-- Quality Policy Card -->
             <div class="max-w-sm bg-white border-4 border-gray-200 rounded-2xl shadow-2xl dark:bg-gray-800 dark:border-gray-700">
-                <img class="rounded-t-lg w-full" src="/Reposync-main/src/assets/3.png" alt="Vision Image" />
+                <img class="rounded-t-lg w-full" src="assets/3.png" alt="Vision Image" />
                 <div class="p-5">
                     <p class="mb-3 font-mono text-gray-700 dark:text-gray-400 text-justify">
                     We Commit to the highest standards of education, value our stakeholders, Strive for continual improvement of our products and services, and Uphold the University’s tenets of Truth, Excellence, and Service to produce globally competitive and morally upright individuals.
